060b4355a0e1abb9ddbeb4f49ff42fff3be311c5
AtomCrafty <atomcrafty@frucost.net>
2021-04-25 22:35:31 +0200

    Add string (un)escape helpers

--- Core/TextHelpers.cs
+++ Core/TextHelpers.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace VToolBase.Core {
 	public static class TextHelpers {
@@ -8,5 +9,24 @@ namespace VToolBase.Core {
 
 		public static string UcWords(this string str) =>
 			str.Split(' ').Select(UcFirst).JoinString(", ");
+
+		public static string Escape(this string value) {
+			return value.Replace(@"\", @"\\").Replace(@"""", @"\""").Replace("\n", @"\n").Replace("\r", @"\r").Replace("\t", @"\t");
+		}
+
+		private static readonly Regex UnescapeRegex = new Regex(@"\\(.)");
+
+		public static string Unescape(this string value) {
+			return UnescapeRegex.Replace(value, match => {
+				switch(match.Groups[1].Value[0]) {
+					case '\\': return "\\";
+					case 'r': return "\r";
+					case 'n': return "\n";
+					case 't': return "\t";
+					case '"': return "\"";
+					default: return match.Groups[1].Value;
+				}
+			});
+		}
 	}
 }
