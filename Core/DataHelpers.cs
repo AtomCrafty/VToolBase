3b6610dad5653b60ce5ed2ac98e09ca465cad564
AtomCrafty <atomcrafty@frucost.net>
2021-04-25 22:35:03 +0200

    Add reader/writer extension methods

--- Core/DataHelpers.cs
+++ Core/DataHelpers.cs
@@ -1,5 +1,12 @@
-﻿namespace VToolBase.Core {
+﻿using System.IO;
+
+namespace VToolBase.Core {
 	public static class DataHelpers {
 
+		public static BinaryReader NewReader(this Stream s) => new BinaryReader(s);
+		public static BinaryWriter NewWriter(this Stream s) => new BinaryWriter(s);
+		
+		public static TextReader NewTextReader(this Stream s) => new StreamReader(s);
+		public static TextWriter NewTextWriter(this Stream s) => new StreamWriter(s);
 	}
 }
