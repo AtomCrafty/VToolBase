0252746a82e1a7d05546e4be6afb118f7c89131a
AtomCrafty <atomcrafty@frucost.net>
2021-06-05 00:34:09 +0200

    Add SeekToo extension method

--- Core/DataHelpers.cs
+++ Core/DataHelpers.cs
@@ -8,5 +8,10 @@ namespace VToolBase.Core {
 		
 		public static TextReader NewTextReader(this Stream s) => new StreamReader(s);
 		public static TextWriter NewTextWriter(this Stream s) => new StreamWriter(s);
+
+		public static void SeekTo(this BinaryReader reader, long position) =>
+			reader.BaseStream.Seek(position, SeekOrigin.Begin);
+		public static void SeekTo(this BinaryWriter writer, long position) =>
+			writer.BaseStream.Seek(position, SeekOrigin.Begin);
 	}
 }
